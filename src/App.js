d8055cbc01845211e5a69d34eae20144c1ad9133
John Mark Goodson <john.mark.hat@gmail.com>
2020-01-01 21:22:34 -0500

    addedfahrenheit and celcius conversion, started push call results to currentWeather object

--- src/App.js
+++ src/App.js
@@ -3,15 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 
 
+// kelvin to fahrenhiet
+// (k - 273.15) * 9/5 + 32 = F
 
+// kelvin to celcius
+// k - 273.15 = C
+
+// run convert to celcius
+// then run convert to fahrenheit if needed
 
 
 function App() {
   const key = '8f098395e4b8a5cafd82f5af39e50048'
-  console.log(key);
   const callAPI = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=" + key;
-  console.log(callAPI); 
+  // need to get city location from user and convert to id somehow.
 
+  function convertToCelcius(num) {
+    return Math.round(num - 273.15);
+  }
+
+  function convertToFahrenheit(num) {
+    return Math.round(num * 9/5 + 32);
+  }
+
+  let currentWeather = {
+    temp: 30,
+    type: "sunny",
+    // etc etc.
+  }
   fetch (callAPI, {mode: 'cors'}) 
     .then(function(response) {
       return response.json();
@@ -21,8 +40,18 @@ function App() {
       return main;
     })
     .then(function(main) {
-      console.log("It is is ", main.temp);
+      let converted = convertToCelcius(main.temp)
+      converted = convertToFahrenheit(converted)
+      console.log("It is is ", converted, " degrees and " + currentWeather.type);
+      currentWeather.temp = converted;
     })
+
+  console.log(currentWeather)
+
+  // Ok api call can get temperature, need to grab other relevant data
+  // then set that data to an object
+  // then paint that object onto screen ?
+  // store object in state in react?
 
   const sampel = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY} "
   return (
