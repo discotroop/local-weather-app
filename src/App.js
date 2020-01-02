1e8fc219559ad050e8e5ab3125369b5a3ae2805a
John Mark Goodson <john.mark.hat@gmail.com>
2020-01-01 20:54:51 -0500

    set api call, working on massaging data.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
+
 function App() {
+  const key = '8f098395e4b8a5cafd82f5af39e50048'
+  console.log(key);
+  const callAPI = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=" + key;
+  console.log(callAPI); 
+
+  fetch (callAPI, {mode: 'cors'}) 
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function (response) {
+      const main = response.list[0].main;
+      return main;
+    })
+    .then(function(main) {
+      console.log("It is is ", main.temp);
+    })
+
+  const sampel = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY} "
   return (
     <div className="App">
       <header className="App-header">
