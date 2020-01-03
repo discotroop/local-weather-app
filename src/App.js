38f57d48b4de7927ae2d66c439d962a4784d4c92
John Mark Goodson <john.mark.hat@gmail.com>
2020-01-02 21:10:54 -0500

    wrestling api calls and state.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-
-// kelvin to fahrenhiet
-// (k - 273.15) * 9/5 + 32 = F
-
-// kelvin to celcius
-// k - 273.15 = C
-
-// run convert to celcius
-// then run convert to fahrenheit if needed
-
+import { thisExpression } from '@babel/types';
 
 function App() {
   const key = '8f098395e4b8a5cafd82f5af39e50048'
@@ -26,26 +15,31 @@ function App() {
     return Math.round(num * 9/5 + 32);
   }
 
-  let currentWeather = {
-    temp: 30,
-    humidity: "",
-    wind: "",
-    condition: "sunny",
-    // etc etc.
-  }
+  function getWeather () {
   fetch (callAPI, {mode: 'cors'}) 
     .then(function(response) {
       return response.json();
     })
     .then(function (response) {
-      // get data
+      // create object 
+      let currentWeather = {
+        temp: 30,
+        humidity: 5,
+        wind: 5,
+        condition: "sunny",
+        high: 0,
+        low: 0
+      }
+      // set data
       const main = response.list[0].main;
       const condition = response.list[0].weather[0].description;
       const humidity = response.list[0].main.humidity;
-      const wind = response.list[0].wind.speed
+      const wind = response.list[0].wind.speed;
 
       // set current weather
       currentWeather.temp = convertToFahrenheit(convertToCelcius(main.temp))
+      currentWeather.low = convertToFahrenheit(convertToCelcius(main.temp_min));
+      currentWeather.high = convertToFahrenheit(convertToCelcius(main.temp_max));
       currentWeather.condition = condition;
       currentWeather.humidity = humidity;
       currentWeather.wind = wind;
@@ -53,34 +47,100 @@ function App() {
       return (currentWeather);
     })
     .then(function(currentWeather) {
-      console.log(currentWeather)
+      console.log(currentWeather, "inside fetch")
+      return currentWeather;
     })
+  }
 
-  console.log(currentWeather)
+  function testing (props) {
+    console.log("testing")
+  }
 
-  // Ok api call can get temperature, need to grab other relevant data
-    // temp, humidity, wind, clear/cloudy/etc.
-  // then set that data to an object
-  // then paint that object onto screen ?
-  // store object in state in react?
+  class Weather extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        weather: {
+          temp: 30,
+          humidity: 5,
+          wind: 5,
+          condition: "sunny",
+          high: 0,
+          low: 0
+        }
+      }
+    }
 
-  const sampel = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY} "
+    componentDidMount () {
+      testing();
+      let current = getWeather();
+      console.log(current);
+      // might need to move this back out of the component come to think
+      // of it... just to get user input on location ? 
+      const key = '8f098395e4b8a5cafd82f5af39e50048'
+      let locationID =  "524901";
+      const callAPI = `http://api.openweathermap.org/data/2.5/forecast?id=${locationID}&APPID=${key}`;
+      // need to get city location from user and convert to id somehow.
+    
+      function convertToCelcius(num) {
+        return Math.round(num - 273.15);
+      }
+      function convertToFahrenheit(num) {
+        return Math.round(num * 9/5 + 32);
+      }
+      // const that = this;
+
+      fetch (callAPI, {mode: 'cors'}) 
+      .then(function(response) {
+        return response.json();
+      })
+      .then(response => {
+        // create object 
+        let currentWeather = {
+          temp: 30,
+          humidity: 5,
+          wind: 5,
+          condition: "sunny",
+          high: 0,
+          low: 0
+        }
+        // set data
+        const main = response.list[0].main;
+        const condition = response.list[0].weather[0].description;
+        const humidity = response.list[0].main.humidity;
+        const wind = response.list[0].wind.speed;
+  
+        // set current weather
+        currentWeather.temp = convertToFahrenheit(convertToCelcius(main.temp))
+        currentWeather.low = convertToFahrenheit(convertToCelcius(main.temp_min));
+        currentWeather.high = convertToFahrenheit(convertToCelcius(main.temp_max));
+        currentWeather.condition = condition;
+        currentWeather.humidity = humidity;
+        currentWeather.wind = wind;
+  
+        return currentWeather;
+      })
+      .then (currentWeather => {
+        this.setState({ weather: currentWeather});
+      });
+    }
+    
+    render () {
+      return (
+        <div>
+          <div> testing </div>
+          <div> current temp {this.state.weather.temp} </div>
+          <div> low {this.state.weather.low} </div>
+          <div> high {this.state.weather.high} </div>
+          <div> {this.state.weather.condition} </div>
+          <div> wind {this.state.weather.wind} </div>
+        </div>
+      );
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Weather />
     </div>
   );
 }
